f4fe1e78c4eb7d005dc6747d853dcdab14eb7802
Sergio Ruiz <serginator@gmail.com>
2013-04-25 01:32:01 +0200

    Removed an unnecesary test

--- spec/passwordValidator_spec.js
+++ spec/passwordValidator_spec.js
@@ -18,10 +18,6 @@ describe("password test", function() {
 		expect(isValidPassword("abcddfdf")).toBe(false);
 	});
 
-	it("al menos una letra", function() {
-		expect(isValidPassword("1234")).toBe(false);
-	});
-
 	it("al menos dos letras con una mayúscula", function() {
 		expect(isValidPassword("aa1234")).toBe(false);
 	});
@@ -30,74 +26,3 @@ describe("password test", function() {
 		expect(isValidPassword("AA1234")).toBe(false);
 	});
 });
-
-var validPassword = {
-	validarLongitud: function(password){
-		if (password.length > 4) {
-			return true;
-		}
-
-		return false;
-	},
-
-	tieneAlMenosUnNumero: function(password){
-		if (/\d/.test(password)) {
-			return true;
-		}
-
-		return false;
-	},
-
-	tieneAlMenosUnaLetra: function(password){
-		if(/[a-zA-Z]/i.test(password)) {
-			return true;
-		}
-
-		return false;
-	},
-
-	tieneAlmenosUnaMayuscula: function(password){
-		if(/[A-Z]/.test(password)) {
-			return true;
-		}
-
-		return false;
-	},
-
-	tieneAlmenosUnaMinuscula: function(password){
-		if(/[a-z]/.test(password)) {
-			return true;
-		}
-
-		return false;
-	}
-};
-
-var isValidPassword = function(password){
-	if(validPassword.validarLongitud(password) &&
-		validPassword.tieneAlMenosUnNumero(password) &&
-		validPassword.tieneAlMenosUnaLetra(password) &&
-		validPassword.tieneAlmenosUnaMayuscula(password) &&
-		validPassword.tieneAlmenosUnaMinuscula(password)
-		){
-		return true;
-	}else{
-		return false;
-	}
-}
-
-
-
-/*
-var isValidPassword = function(password) {
-	if (password.length < 4) {
-		return false;
-	} else {
-		if (/\d/.test(password) && /[a-z]/i.test(password) && /[A-Z]/.test(password)) {
-			return true;
-		} else {
-			return false;
-		}
-		return true;
-	}
-};*/
