7f00f4f37d2b935c068a2c5ea37c703cddb12b01
Sergio Ruiz <serginator@gmail.com>
2013-04-25 01:46:13 +0200

    Green: More cleanning

--- src/passwordValidator.js
+++ src/passwordValidator.js
@@ -1,53 +1,39 @@
 var validPassword = {
-	validarLongitud: function(password){
+	validarLongitud: function(password) {
 		if (password.length > 4) {
 			return true;
 		}
-
 		return false;
 	},
 
-	tieneAlMenosUnNumero: function(password){
+	tieneAlMenosUnNumero: function(password) {
 		if (/\d/.test(password)) {
 			return true;
 		}
-
 		return false;
 	},
 
-	tieneAlMenosUnaLetra: function(password){
-		if(/[a-zA-Z]/i.test(password)) {
+	tieneAlmenosUnaMayuscula: function(password) {
+		if (/[A-Z]/.test(password)) {
 			return true;
 		}
-
 		return false;
 	},
 
-	tieneAlmenosUnaMayuscula: function(password){
-		if(/[A-Z]/.test(password)) {
-			return true;
-		}
-
-		return false;
-	},
-
-	tieneAlmenosUnaMinuscula: function(password){
+	tieneAlmenosUnaMinuscula: function(password) {
 		if(/[a-z]/.test(password)) {
 			return true;
 		}
-
 		return false;
 	}
 };
 
-var isValidPassword = function(password){
+var isValidPassword = function(password) {
 	if (validPassword.validarLongitud(password) &&
 			validPassword.tieneAlMenosUnNumero(password) &&
-			validPassword.tieneAlMenosUnaLetra(password) &&
 			validPassword.tieneAlmenosUnaMayuscula(password) &&
 			validPassword.tieneAlmenosUnaMinuscula(password)) {
 		return true;
-	} else {
-		return false;
 	}
+	return false;
 };
