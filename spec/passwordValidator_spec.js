4f4ecc81852b270390199a8b18f7a3651d9b5d20
Sergio Ruiz <serginator@gmail.com>
2013-04-24 16:26:18 +0200

    fail

--- spec/passwordValidator_spec.js
+++ spec/passwordValidator_spec.js
@@ -1,5 +1,12 @@
+/**
+ * Al menos 4 carácteres
+ * Al menos un número y una letra
+ * Al menos una minúscula
+ * Al menos una mayúscula
+ */
+
 describe("test", function(){
-	it("test", function(){
-		
+	it("longitud minima de seguridad", function(){
+		expect(isValidPassword("1234567jjkh")).toBe(true);
 	});
 });
